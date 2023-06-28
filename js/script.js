f1e55076a3e0efe92386e3f23c49b7a42ba49fc3
molly_b <brownmolly109@gmail.com>
2023-06-28 11:27:05 -0400

    display word and guessed letters

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 //global variables
 
 //unordered list where the player's guessed letters will appear
-const pastGuessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 
 //the button with the text "Guess!" in it
 const guessButton = document.querySelector(".guess");
 
 //The text input where the player will guess a letter
-const textInput = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 
 //The empty paragraph where the word in progress will appear
 const wordInProgress = document.querySelector(".word-in-progress");/*how should I refer to the p class?*/
 
 //The paragraph where the remaining guesses will display
-const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesElement = document.querySelector(".remaining");
 
 //The span inside the paragraph where the remaining guesses will display
 const remainingGuessesSpan = document.querySelector(".remaining span");/*double check this*/
 
 //The empty paragraph where messages will appear when the player guesses a letter
-const messageWillAppear = document.querySelector(".message");
+const message = document.querySelector(".message");
 
 //The hidden button that will appear prompting the player to play again.
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
-
 //add a new global variable for player guesses
 const guessedLetters = [];
 
@@ -48,34 +47,33 @@ circlesPlaceholders(word);
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     //Empty message paragraph
-    messageWillAppear.innerText = "";
+    message.innerText = "";
     //grab what was entered in the input
-    const guess = textInput.value;
+    const guess = letterInput.value;
     //make sure it's a single letter
     const acceptedGuess = validateInput(guess);
 
     if (acceptedGuess) {
         makeGuess(guess);
     }
-    textInput.value = "";
+    letterInput.value = "";
 });
 
 //Create a function to check play's input
-const validateInput = function (input){
+const validateInput = function (input) {
                             //why is input used instead of textInput.value
     const acceptedLetter = /[a-zA-Z]/;
     if (input.length === 0) {
         //Is the input empty?
-        messageWillAppear.innerText ="Please guess a letter.";
-    }
-    else if (input.length > 1) {
+        message.innerText ="Please enter a letter.";
+    } else if (input.length > 1) {
         //Did you type more than one letter?
-        messageWillAppear.innerText = "Please enter one letter at a time.";
+        message.innerText = "Please enter a single letter.";
     }
     //use the /match() method here to check if they've entered a character that doesn't match the regular expression
     else if (!input.match(acceptedLetter)) {
         //Did you type a number, a special character or someother non letter thiing?
-        messageWillAppear.innerText = "Please enter a letter from A to Z.";
+        message.innerText = "Please enter a letter from A to Z.";
     } else {
         //a single letter has been input
         return input;
@@ -87,10 +85,44 @@ const makeGuess = function (guess) {
     //JavaScript is case sensitive- convert all letters to one casing
     guess = guess.toUpperCase();
     if (guessedLetters.includes(guess)) {
-        messageWillAppear.innerText = "You already guessed this letter. Try again!";
+        message.innerText = "You already guessed this letter. Try again!";
     } else { 
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        displayGuessedLetters();
+        updateWordInProgress(guessedLetters);
     }
 };
 
+//Step 4 of 6: Display Word and Guessed Letters (will use split() and join() )
+
+//Create a function to show the guessed letters
+const displayGuessedLetters = function () {
+    //to clear the list
+    guessedLettersElement.innerHTML = "";
+    //to create a new list item for each letter inside your guessedLetters array
+    //how do you know to use a for...of loop?
+    for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    //what does this next step do?
+    li.innerText = letter;
+    guessedLettersElement.append(li);
+    }
+};
+
+//Create a function to update the word in progress
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+    //why isn't guessed letters used (const guessedLetters of wordArray)
+    if (guessedLetters.includes(letter)) {
+        revealWord.push(letter.toUpperCase());
+        } else {
+        revealWord.push("●");
+        }
+    }
+};
+
+//Create a function to check if the player won
