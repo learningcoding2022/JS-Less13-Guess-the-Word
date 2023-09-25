bfc15e63d79ede4c7bf39c0dde7b105ceffe9335
molly_b <brownmolly109@gmail.com>
2023-09-25 11:44:40 -0400

    removed comments

--- js/script.js
+++ js/script.js
@@ -25,13 +25,12 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 let word = "magnolia";
-//why is this const changed to let
+
 //add a new global variable for player guesses
 const guessedLetters = [];
 
 //create a global variable called remainingGuesses and set it to a value of 8
 let remainingGuesses = 8;
-//why is this let? and why will the value change?
 
 //add an async function
 const getWord = async function () {
@@ -45,14 +44,11 @@ const getWord = async function () {
 };
 
 getWord();
-//Where do the results of getWord(); show up? I didn't see anything in the console of dev tools.
 
 //write a function to add placeholders for each letter
 const circlesPlaceholders = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
-        //console.log(letter);
-        //why push the placeholderLetters?
         placeholderLetters.push("●");
     }
     wordInProgress.innerText = placeholderLetters.join("");
@@ -78,18 +74,15 @@ guessButton.addEventListener("click", function (e) {
 
 //Create a function to check play's input
 const validateInput = function (input) {
-                            //why is input used instead of textInput.value
+                            
     const acceptedLetter = /[a-zA-Z]/;
     if (input.length === 0) {
-        //Is the input empty?
         message.innerText ="Please enter a letter.";
     } else if (input.length > 1) {
-        //Did you type more than one letter?
         message.innerText = "Please enter a single letter.";
     }
     //use the /match() method here to check if they've entered a character that doesn't match the regular expression
     else if (!input.match(acceptedLetter)) {
-        //Did you type a number, a special character or someother non letter thiing?
         message.innerText = "Please enter a letter from A to Z.";
     } else {
         //a single letter has been input
@@ -119,10 +112,8 @@ const displayGuessedLetters = function () {
     //to clear the list
     guessedLettersElement.innerHTML = "";
     //to create a new list item for each letter inside your guessedLetters array
-    //how do you know to use a for...of loop?
     for (const letter of guessedLetters) {
     const li = document.createElement("li");
-    //what does this next step do?
     li.innerText = letter;
     guessedLettersElement.append(li);
     }
@@ -134,7 +125,6 @@ const updateWordInProgress = function (guessedLetters) {
     const wordArray = wordUpper.split("");
     const revealWord = [];
     for (const letter of wordArray) {
-    //why isn't guessed letters used (const guessedLetters of wordArray)
     if (guessedLetters.includes(letter)) {
         revealWord.push(letter.toUpperCase());
         } else {
@@ -182,7 +172,7 @@ const startOver = function () {
     guessButton.classList.add("hide");
     remainingGuessesElement.classList.add("hide");
     guessedLettersElement.classList.add("hide");
-    //to shoe the button to play again
+    //to show the button to play again
     playAgainButton.classList.remove("hide");
 };
 
